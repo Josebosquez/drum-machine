636268f5b7058565d90fb81c2ced0ee8956d1c92
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-04-05 14:00:55 -0600

    2b if unchecked, stops playing tock/tick

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
 const tock = new Audio('sounds/tock.mp3');
+// checked metronome box
+document.querySelector("#metronome").checked = true;
 
 let count = 0;
 
 // This function is called every 600ms
 function update() {
-    // Play the 'tick' sound
-    tick.play();
-    // capture query html element for counter
-    const metronomeCount = document.querySelector(".metronome-count")
-    metronomeCount.innerText = count;
-    
-    // Play the "tock sound"
-    if(count % 4 === 0){
-    tock.play();
+    if (document.querySelector("#metronome").checked === true){
+
+        // Play the 'tick' sound
+        tick.play();
+        // capture query html element for counter
+        const metronomeCount = document.querySelector(".metronome-count")
+        metronomeCount.innerText = count;
+        
+        // Play the "tock sound"
+        if(count % 4 === 0){
+            tock.play();
+        }
+        // count updating
+        if (count <= 3){
+            count++;
+            // reset the count
+        } else if (count = 4){
+            count = 1
+        }
     }
-    // count updating
-    if (count <= 3){
-        count++;
-    // reset the count
-    } else if (count = 4){
-        count = 1
-    }
     
     
-    // checked metronome box
-    document.querySelector("#metronome").checked = true;
 
     }
 
